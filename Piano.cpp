46629f6ae6715f94f7884318fe08e151a6789885
humblehwang <z8630076@yahoo.com.tw>
2018-03-23 16:08:35 +0800

    上船測試

---
+++ Piano.cpp
@@ -0,0 +1,136 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include<string.h>
+#include<conio.h> 
+#include <windows.h> 
+
+
+int main(void) 
+{ 
+
+
+int tick=300,tick2=600;
+
+/*char input;
+while(1){
+	input=getch();
+	switch(input[i]){
+		case'q':Beep(147,tick);break;//5
+		case'e':Beep(175,tick);break;//6
+		case'y':Beep(247,tick);break;//7
+		case'u':Beep(262,tick);break;//1
+		case'i':Beep(294,tick);break;//2
+		case'o':Beep(330,tick);break;//3
+		case'p':Beep(349,tick);break;//4
+		case'a':Beep(392,tick);break;//5
+		case's':Beep(440,tick);break;//6
+		case'd':Beep(494,tick);break;//7
+		case'f':Beep(523,tick);break;//1
+		case'g':Beep(587,tick);break;//2
+		case'h':Beep(659,tick);break;//3
+		case'j':Beep(698,tick);break;//4
+		case'k':Beep(784,tick);break;//5
+		case'l':Beep(880,tick);break;//6
+		case';':Beep(980,tick);break;//7
+		case'z':Beep(1060,tick);break;//1
+		case'x':Beep(1175,tick);break;//2	
+	    case'c':Beep(1318,tick);break;//3
+		case'v':Beep(1397,tick);break;//4
+		case'b':Beep(1568,tick);break;//5
+		case'n':Beep(1760,tick);break;//6
+		case'm':Beep(1976,tick);break;//7		
+	}//switch
+}
+*/
+
+//Happy birthday
+
+Beep(784,tick);
+Beep(784,tick);
+Beep(880,tick);
+Beep(784,tick);
+Beep(1060,tick);
+Beep(980,tick2);
+
+Beep(784,tick);
+Beep(784,tick);
+Beep(880,tick);
+Beep(784,tick);
+Beep(1175,tick);
+Beep(1060,tick2);
+
+Beep(784,tick);
+Beep(784,tick);
+Beep(1568,tick);
+Beep(1318,tick);
+Beep(1060,tick);
+Beep(980,tick);
+Beep(880,tick2);
+
+Beep(1397,tick);
+Beep(1397,tick);
+Beep(1318,tick);
+Beep(1060,tick);
+Beep(1175,tick);
+Beep(1060,tick2);
+
+//¤p»e¸Á 
+
+Beep(784,tick);
+Beep(659,tick);
+Beep(659,tick2);
+Beep(698,tick);
+Beep(587,tick);
+Beep(587,tick2);
+Beep(523,tick);
+Beep(587,tick);
+Beep(659,tick);
+Beep(698,tick);
+Beep(784,tick);
+Beep(784,tick);
+Beep(784,tick2);
+
+
+Beep(784,tick);
+Beep(659,tick);
+Beep(659,tick2);
+Beep(698,tick);
+Beep(587,tick);
+Beep(587,tick2);
+Beep(523,tick);
+Beep(659,tick);
+Beep(784,tick);
+Beep(784,tick);
+Beep(523,tick2);
+
+
+Beep(587,tick);
+Beep(587,tick);
+Beep(587,tick);
+Beep(587,tick);
+Beep(587,tick);
+Beep(659,tick);
+Beep(698,tick2);
+
+Beep(659,tick);
+Beep(659,tick);
+Beep(659,tick);
+Beep(659,tick);
+Beep(659,tick);
+Beep(698,tick);
+Beep(784,tick2);
+
+Beep(784,tick);
+Beep(659,tick);
+Beep(659,tick2);
+Beep(698,tick);
+Beep(587,tick);
+Beep(587,tick2);
+Beep(523,tick);
+Beep(659,tick);
+Beep(784,tick);
+Beep(784,tick);
+Beep(523,tick2);
+
+return 0; 
+} 
